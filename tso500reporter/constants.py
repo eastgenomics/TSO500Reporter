044acd8419aaae82a9cec0fb44ab12654d8fad4f
spaul91 <sophie.paul@addenbrookes.nhs.uk>
2021-11-19 10:53:24 +0000

    removed whitespace; added docstring

--- tso500reporter/constants.py
+++ tso500reporter/constants.py
@@ -1,6 +1,9 @@
-MSI_FIELDS = ["Usable MSI Sites", 
-        "Total MSI Sites Unstable", 
+"""
+Constants for use by module
+"""
+MSI_FIELDS = ["Usable MSI Sites",
+        "Total MSI Sites Unstable",
         "Percent Unstable MSI Sites"]
-TMB_FIELDS = ["Total TMB", 
-        "Coding Region Size in Megabases", 
+TMB_FIELDS = ["Total TMB",
+        "Coding Region Size in Megabases",
         "Number of Passing Eligible Variants"]
